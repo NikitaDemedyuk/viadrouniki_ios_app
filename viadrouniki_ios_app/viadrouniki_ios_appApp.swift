d13489ad883cb0c628e2f12c81ababde4a530515
Nikita Demedyuk <nikitademedyuk1@gmail.com>
2026-07-07 10:52:39 +0300

    Initial Commit

---
+++ viadrouniki_ios_app/viadrouniki_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  viadrouniki_ios_appApp.swift
+//  viadrouniki_ios_app
+//
+//  Created by Никита on 06.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct viadrouniki_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
